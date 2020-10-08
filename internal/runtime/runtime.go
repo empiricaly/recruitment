854d37bbcd7f3e090c28a0beabb630c2dce710f9
Nicolas Paton <nicolas.paton@gmail.com>
2020-10-08 15:40:53 +0800

    wip: runtime add runs

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -2,12 +2,11 @@ package runtime
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/empiricaly/recruitment/internal/ent"
 	"github.com/empiricaly/recruitment/internal/ent/hook"
-	"github.com/empiricaly/recruitment/internal/ent/run"
+	runModel "github.com/empiricaly/recruitment/internal/ent/run"
 	"github.com/empiricaly/recruitment/internal/storage"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
@@ -19,8 +18,8 @@ type Runtime struct {
 	// db conn
 	conn *storage.Conn
 
-	// runs tracked by the runtime. A map of Run ID to *Run.
-	runs map[string]*ent.Run
+	// runs tracked by the runtime. The map key is the Run ID.
+	runs map[string]*runState
 
 	// update wants Run IDs to update the runtime
 	// When a new Run ID comes in, it is fetched from the DB, then
@@ -36,7 +35,7 @@ type Runtime struct {
 func Start(conn *storage.Conn) (*Runtime, error) {
 	r := &Runtime{
 		conn:    conn,
-		runs:    make(map[string]*ent.Run),
+		runs:    make(map[string]*runState),
 		updates: make(chan string),
 	}
 	go r.processRuns()
@@ -55,10 +54,63 @@ func (r *Runtime) Stop() {
 	// stop run loop here?
 }
 
+type runState struct {
+	run   *ent.Run
+	timer *time.Timer
+}
+
+func (r *Runtime) addRun(run *ent.Run) {
+	_, ok := r.runs[run.ID]
+	if ok {
+		log.Warn().Msgf("run %s already tracked", run.ID)
+		return
+	}
+
+	if run.Status == runModel.StatusCREATED {
+
+	}
+}
+
+func (r *Runtime) removeRun(run *ent.Run) {
+	state, ok := r.runs[run.ID]
+	if !ok {
+		return
+	}
+
+	if !state.timer.Stop() {
+		<-state.timer.C
+	}
+}
+
+func (r *Runtime) processRun(runID string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	runRecord, err := r.conn.Run.Get(ctx, runID)
+	if err != nil {
+		if ent.IsNotFound(err) {
+			r.removeRun(runRecord)
+		} else if ent.IsNotSingular(err) {
+			log.Error().Err(err).Msg("multiple runs with same ID!")
+		} else {
+			log.Error().Err(err).Msg("updating Run state in runtime")
+		}
+		return
+	}
+
+	switch runRecord.Status {
+	case runModel.StatusDONE, runModel.StatusFAILED, runModel.StatusTERMINATED, runModel.StatusPAUSED:
+		r.removeRun(runRecord)
+	case runModel.StatusCREATED, runModel.StatusRUNNING:
+		r.addRun(runRecord)
+	default:
+		log.Error().Msgf("unknown run status: %s", runRecord.Status.String())
+	}
+}
+
 func (r *Runtime) processRuns() {
 	for {
 		runID := <-r.updates
-		fmt.Printf("\n\n\n\n\n\nUpdate run: %s\n\n\n\n", runID)
+		r.processRun(runID)
 	}
 }
 
@@ -89,8 +141,8 @@ func (r *Runtime) registerExistingSteps() error {
 	defer cancel()
 
 	runIDs, err := r.conn.Run.Query().
-		Where(run.StatusIn(run.StatusCREATED, run.StatusRUNNING, run.StatusPAUSED)).
-		Select(run.FieldID).
+		Where(runModel.StatusIn(runModel.StatusCREATED, runModel.StatusRUNNING)).
+		Select(runModel.FieldID).
 		Strings(ctx)
 	if err != nil {
 		return errors.Wrap(err, "initialize exisitng runs")
